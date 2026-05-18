135a0c0db40d1a5e634e73a9d286247239d884bf
Иван <samotoylovivan@mail.ru>
2026-05-18 23:26:01 +0600

    add work wear

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "ru.sv.wear"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "ru.sv.wear"
+        applicationId = "ru.sv.personaltrainer"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
